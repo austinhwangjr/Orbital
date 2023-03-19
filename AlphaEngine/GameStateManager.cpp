3a1fd7663c7d6360ad4bdbe21314b7c154865a14
ongyouyang <93074650+ongyouyang@users.noreply.github.com>
2023-03-19 12:18:11 +0800

    Lose Condition implemented
    Lose Condition implemented

--- AlphaEngine/GameStateManager.cpp
+++ AlphaEngine/GameStateManager.cpp
@@ -21,6 +21,7 @@ Technology is prohibited.
 #include "HowToPlay.h"
 #include "Credits.h"
 #include "Highscore.h"
+#include "LoseMenu.h"
 #include "Options.h"
 #include <functional>
 #include <iostream>
@@ -106,6 +107,14 @@ void gsm::update()
 		fpFree = Options::free;
 		fpUnload = Options::unload;
 		break;
+	//case GS_LOSEMENU:
+	//	fpLoad = lose_menu::load;
+	//	fpInit = lose_menu::init;
+	//	fpUpdate = lose_menu::update;
+	//	fpDraw = lose_menu::draw;
+	//	fpFree = lose_menu::free;
+	//	fpUnload = lose_menu::unload;
+	//	break;
 
 
 
--- AlphaEngine/LoseMenu.h
+++ AlphaEngine/LoseMenu.h
@@ -1,11 +1,20 @@
 #pragma once
+#include "AEEngine.h"
 
 namespace lose_menu
 {
-	void load();
-	void init();
-	void update();
-	void draw();
-	void free();
-	void unload();
+    void load();
+    void init();
+    void update();
+    void draw(const AEVec2& camPos);
+    void free();
+    void unload();
+
+    struct lm_Button
+    {
+        float x;
+        float y;
+        float width;
+        float height;
+    };
 }
