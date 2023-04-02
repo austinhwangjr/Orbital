ab33d03e02a085f90c85bad8d5f0965e874a0d12
ongyouyang <93074650+ongyouyang@users.noreply.github.com>
2023-04-02 19:31:06 +0800

    fix bug with mesh
    fix bug with mesh

--- AlphaEngine/Graphics.cpp
+++ AlphaEngine/Graphics.cpp
@@ -121,14 +121,14 @@ void Rendering::GameLevelSquareMesh(AEGfxVertexList*& pMesh)
     // Color parameters represent colours as ARGB
     // UV coordinates to read from loaded textures
     AEGfxTriAdd(
-        -0.5f, -0.5f, 0xFFFF00FF, 0.0f, 0.0f,
-         0.5f, -0.5f, 0xFFFFFF00, 1.0f, 0.0f,
-        -0.5f,  0.5f, 0xFF00FFFF, 0.0f, 1.0f);
+        -0.5f, -0.5f, 0xFFFF00FF, 1.0f, 0.0f,
+        0.5f, -0.5f, 0xFFFFFF00, 0.0f, 0.0f,  
+        -0.5f, 0.5f, 0xFF00FFFF, 1.0f, 1.0f);
 
     AEGfxTriAdd(
-         0.5f, -0.5f, 0xFFFFFFFF, 1.0f, 0.0f,
-         0.5f,  0.5f, 0xFFFFFFFF, 1.0f, 1.0f,
-        -0.5f,  0.5f, 0xFFFFFFFF, 0.0f, 1.0f);
+        0.5f, -0.5f, 0xFFFFFFFF, 0.0f, 0.0f,  
+        0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0xFFFFFFFF, 1.0f, 1.0f);
 
     // Saving the mesh (list of triangles) in pMesh 
     pMesh = AEGfxMeshEnd();
